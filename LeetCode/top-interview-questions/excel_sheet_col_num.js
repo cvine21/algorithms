c55dc2658e62ea39b1081db94fe9b6e57602448b
Claudine Vine <rafarakh@gmail.com>
2022-09-26 11:09:02 +0300

    feat: new prob solved

--- LeetCode/top-interview-questions/excel_sheet_col_num.js
+++ LeetCode/top-interview-questions/excel_sheet_col_num.js
@@ -16,29 +16,46 @@ For example:
  * @param {string} columnTitle
  * @return {number}
  */
+// var titleToNumber = function (columnTitle) {
+// 	const a = "A".charCodeAt(0);
+// 	const z = "Z".charCodeAt(0);
+// 	const alphRange = z - a;
+
+// 	let num = 0;
+// 	const len = columnTitle.length;
+
+// 	for (let i = 0; i < len; ++i) {
+// 		const current = columnTitle[i].charCodeAt(0) - a + 1;
+
+// 		// if (i === len - 1) {
+// 		// 	num += current;
+// 		// } else {
+// 			num += current * Math.pow(alphRange + 1, len - (i + 1));
+// 		// }
+// 	}
+
+// 	return num;
+// };
+
 var titleToNumber = function (columnTitle) {
 	const a = "A".charCodeAt(0);
 	const z = "Z".charCodeAt(0);
 	const alphRange = z - a;
 
-	let num = 0;
-	for (let i = columnTitle.length - 1; i >= 0; --i) {
-		num += Math.pow(columnTitle[i].charCodeAt(0) - a + 1, i + 1);
+	let colNum = 0;
+	const len = columnTitle.length;
+
+	for (let i = 0; i < len; ++i) {
+		const curCharCode = columnTitle[i].charCodeAt(0) - a + 1;
+
+		colNum += curCharCode * Math.pow(alphRange + 1, len - (i + 1));
 	}
 
-	return num;
-	// return columnTitle.split("").reduce((accum, cur, i) => {
-
-	// 	return (accum +=
-	// 		columnTitle.length === 1
-	// 			? cur.charCodeAt(0) - a + 1
-	// 			: i == 0
-	// 			? Math.pow(cur.charCodeAt(0) - a + 1, 2)
-	// 			: alphRange + (cur.charCodeAt(0) - a + 1));
-	// }, 0);
+	return colNum;
 };
 
 console.log(titleToNumber("A")); // 1
 console.log(titleToNumber("Z")); // 26
 console.log(titleToNumber("AB")); // 28
 console.log(titleToNumber("ZY")); // 701
+console.log(titleToNumber("FXSHRXW")); // 2147483647
