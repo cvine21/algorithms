c5c0d39a135a34f9b6e6e8e3385a56b4ab8bc3a8
Claudine Vine <rafarakh@gmail.com>
2022-09-27 12:06:21 +0300

    feat: contains_duplicate, valid_anagram probs solved

---
+++ LeetCode/top-interview-questions/valid_anagram.js
@@ -0,0 +1,28 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+	Input: s = "anagram", t = "nagaram"
+	Output: true
+
+Example 2:
+	Input: s = "rat", t = "car"
+	Output: false
+*/
+
+/*
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+	let sortedS = [...s].sort().join("");
+	let sortedT = [...t].sort().join("");
+
+	return sortedS === sortedT;
+};
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("rat", "car"));
