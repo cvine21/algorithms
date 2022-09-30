c519d97af05658ee5f50227294ce1b24e80d5183
Claudine Vine <rafarakh@gmail.com>
2022-09-30 11:32:22 +0300

    feat: intersection_of_arrays prob solved

---
+++ LeetCode/top-interview-questions/intersection_of_arrays.js
@@ -0,0 +1,40 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+
+Example 1:
+
+	Input: nums1 = [1,2,2,1], nums2 = [2,2]
+	Output: [2,2]
+
+Example 2:
+	Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+	Output: [4,9]
+	Explanation: [9,4] is also accepted.
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+	const intersection = [];
+	const shortest = nums1.length <= nums2.length ? nums1 : nums2;
+	const longest = nums1.length > nums2.length ? nums1 : nums2;
+
+	for (let i = 0; i < shortest.length; ++i) {
+		let index = longest.indexOf(shortest[i]);
+
+		if (index !== -1) {
+			intersection.push(...longest.splice(index, 1));
+		}
+	}
+
+	return intersection;
+};
+
+console.log(intersect([1, 2, 2, 1], [2, 2])); // [2, 2]
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])); // [4, 9]
+console.log(intersect([4, 9, 5], [9, 9, 5, 4])); // [4, 9, 5]
+console.log(intersect([3, 1, 2], [1, 1])); // [1]
