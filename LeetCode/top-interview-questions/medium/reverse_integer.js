4e7141d261e90020290f902425cc68dab7931862
cvine <rafarakh@gmail.com>
2022-10-03 10:44:33 +0300

    feat: new medium prob was solved

---
+++ LeetCode/top-interview-questions/medium/reverse_integer.js
@@ -0,0 +1,38 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+
+Example 1:
+	Input: x = 123
+	Output: 321
+
+Example 2:
+	Input: x = -123
+	Output: -321
+
+Example 3:
+	Input: x = 120
+	Output: 21
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var reverse = function(x) {
+    let minRange = Math.pow(-2, 31);
+    let maxRange = Math.pow(2, 31) - 1;
+    let reversed = [...x.toString()].reverse().join('');
+
+    if (x < 0)
+        reversed = '-' + reversed.slice(0, -1);
+    if (reversed >= minRange && reversed <= maxRange)
+       return +reversed;
+
+    return 0;
+};
+
+console.log(reverse(123));
+console.log(reverse(-123));
