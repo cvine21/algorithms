26d79e7a06b4ead078214018c948d750f7fde035
cvine <rafarakh@gmail.com>
2022-10-05 10:45:28 +0300

    feat: new east prob solved

---
+++ LeetCode/top-interview-questions/medium/[UNSOLVED]letter_combinations.js
@@ -0,0 +1,49 @@
+/*
+Letter Combinations of a Phone Number.
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ 
+
+Example 1:
+	Input: digits = "23"
+	Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+	Input: digits = ""
+	Output: []
+Example 3:
+	Input: digits = "2"
+	Output: ["a","b","c"]
+*/
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+	const phone = {
+		2: "abc",
+		3: "def",
+		4: "ghi",
+		5: "jkl",
+		6: "mno",
+		7: "pqrs",
+		8: "tuv",
+		9: "wxyz",
+	};
+
+	if (!digits.length) return [];
+
+	//   const comb = [phone[digits[0]]];
+	//   for (let i = 1; i < digits.length; ++i) {
+	//     comb = comb.map((item) =>
+	//       phone[digits[i]].map((elem) => item + elem).splice()
+	//     );
+	//   }
+	//   console.log(comb);
+	for (digit in digits) {
+	}
+};
+
+letterCombinations("23");
