b2cf3b84bd46f77936329868819f1559eda8ddf3
Claudine Vine <rafarakh@gmail.com>
2022-12-13 14:33:10 +0300

    a new prov

--- LeetCode/LC75/n_ary_tree_preorder_traversal.js
+++ LeetCode/LC75/n_ary_tree_preorder_traversal.js
@@ -1,4 +1,6 @@
 /*
+589. N-ary Tree Preorder Traversal
+
 Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
 
 Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
