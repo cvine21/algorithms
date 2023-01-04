0db9be5334573a881f5602508ec525d49064133e
cvine <rafarakh@gmail.com>
2023-01-04 12:38:57 +0300

    feat: 🎸 new probs
    some new problems's been solved

--- LeetCode/algorithm/number_of_1_bits.js
+++ LeetCode/algorithm/number_of_1_bits.js
@@ -1,10 +1,10 @@
 /*
+191. Number of 1 Bits
 Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
 
 Note:
-
-Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
-In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+  Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+  In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
  
 
 Example 1:
@@ -32,6 +32,14 @@ Follow up: If this function is called many times, how would you optimize it?
  * @param {number} n - a positive integer
  * @return {number}
  */
+
 var hammingWeight = function (n) {
-	return n.toString(2).split("0").join("").length;
+  let res = 0;
+
+  while (n > 0) {
+    res += n % 2;
+    n >>>= 1; // unsigned right shift >>> is like regular >> right shift, but in JS it evaluates left-hand operand as an unsigned number;
+  }
+
+  return res;
 };
